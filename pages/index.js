5dce35971cfea8e8e589a904feb8198eb979cab6
alexotsubo <alex.otsubo@hotmail.com>
2025-11-14 12:58:42 +0000

    teste de index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Zoey Bolinha ou Zoey na Bolinha? Zoey bonitinha!</h1>;
+  return (
+    <h1>Zoey Bolinha ou Zoey na Bolinha? Zoey bonitinha! e muito bonitinha</h1>
+  );
 }
 
 export default Home;
